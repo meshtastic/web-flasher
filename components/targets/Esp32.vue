18917caac6748e258f5454f23ac587df4278e023
Ben Meadors <benmmeadors@gmail.com>
2024-01-02 08:23:14 -0600

    Fixed start over logic

--- components/targets/Esp32.vue
+++ components/targets/Esp32.vue
@@ -73,7 +73,7 @@
                     class="text-black inline-flex w-full justify-center meshtastic-bg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" @click="flash">
                     {{ firmwareStore.$state.shouldCleanInstall ? 'Erase Flash and Install' : 'Update' }}
                 </button>
-                <button v-if="firmwareStore.$state.flashPercentDone > 99"
+                <button v-if="firmwareStore.$state.flashPercentDone > 0 && !firmwareStore.$state.isFlashing"
                     class="text-black inline-flex w-full justify-center meshtastic-bg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" @click="startOver">
                     Start Over
                 </button>
