a557e0d18bddbc10233c3cd5c8db813368256339
Ben Meadors <benmmeadors@gmail.com>
2024-01-03 07:28:35 -0600

    Add favicon and remove old proxy to github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,40 +24,40 @@ export default defineNuxtConfig({
     ],
     server: {
       proxy: {
-        "^/assets/.*": {
-          target: "http://github.com/",
-          changeOrigin: true,
-          followRedirects: true,
-          rewrite: (path) => path.replace(/^\/assets/, ""),
-          headers: {
-            Accept: "application/octet-stream"
-          },
-          configure: (proxy, _options) => {
-            proxy.on("error", (err, _req, _res) => {
-              console.log("proxy error", err);
-            });
-            proxy.on("proxyReq", (proxyReq, req, _res) => {
-              console.log(
-                "Sending Request:",
-                req.method,
-                req.url,
-                " => TO THE TARGET =>  ",
-                proxyReq.method,
-                proxyReq.protocol,
-                proxyReq.host,
-                proxyReq.path,
-              );
-            });
-            proxy.on("proxyRes", (proxyRes, req, _res) => {
-              console.log(
-                "Received Response from the Target:",
-                proxyRes.statusCode,
-                req.url,
-                JSON.stringify(proxyRes.headers),
-              );
-            });
-          },
-        },
+        // "^/assets/.*": {
+        //   target: "http://github.com/",
+        //   changeOrigin: true,
+        //   followRedirects: true,
+        //   rewrite: (path) => path.replace(/^\/assets/, ""),
+        //   headers: {
+        //     Accept: "application/octet-stream"
+        //   },
+        //   configure: (proxy, _options) => {
+        //     proxy.on("error", (err, _req, _res) => {
+        //       console.log("proxy error", err);
+        //     });
+        //     proxy.on("proxyReq", (proxyReq, req, _res) => {
+        //       console.log(
+        //         "Sending Request:",
+        //         req.method,
+        //         req.url,
+        //         " => TO THE TARGET =>  ",
+        //         proxyReq.method,
+        //         proxyReq.protocol,
+        //         proxyReq.host,
+        //         proxyReq.path,
+        //       );
+        //     });
+        //     proxy.on("proxyRes", (proxyRes, req, _res) => {
+        //       console.log(
+        //         "Received Response from the Target:",
+        //         proxyRes.statusCode,
+        //         req.url,
+        //         JSON.stringify(proxyRes.headers),
+        //       );
+        //     });
+        //   },
+        // },
         "^/api/.*": {
           target:
             "https://api.meshtastic.org/",
