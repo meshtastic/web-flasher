ca9dc486a7a0660c9e215fb2869782cf71fbfd9b
Ben Meadors <benmmeadors@gmail.com>
2024-01-14 20:59:59 -0600

    Don't show zip uf2 for selected firmware

--- components/targets/Uf2.vue
+++ components/targets/Uf2.vue
@@ -71,7 +71,7 @@
                     class="text-black inline-flex w-full justify-center bg-meshtastic hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                     Download UF2
                 </a>
-                <button @click="downloadUf2File"
+                <button @click="downloadUf2File" v-else
                     class="text-black inline-flex w-full justify-center bg-meshtastic hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                     Download UF2
                 </button>
