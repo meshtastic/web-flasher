7324dafff97cc2ab702d65529df56d8c8d7a9aee
hdngr <1875033+hdngr@users.noreply.github.com>
2024-02-24 14:20:09 -0800

    add CanaryOne to OfflineDeviceList

--- types/resources.ts
+++ types/resources.ts
@@ -160,6 +160,14 @@ export const OfflineHardwareList = [
         "displayName": "RAK11310"
     },
     {
+        "hwModel": 29,
+        "hwModelSlug": "CANARYONE",
+        "platformioTarget": "canaryone",
+        "architecture": "nrf52840",
+        "activelySupported": true,
+        "displayName": "CanaryOne"
+    },
+    {
         "hwModel": 39,
         "hwModelSlug": "DIY_V1",
         "platformioTarget": "meshtastic-diy-v1",
