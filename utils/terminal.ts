efcdcec60b98ba2cbeb0ebe458a4c2c69e1d400f
Ben Meadors <benmmeadors@gmail.com>
2024-02-27 07:09:28 -0600

    Clear terminal before esptool operation

--- utils/terminal.ts
+++ utils/terminal.ts
@@ -3,5 +3,6 @@ import { Terminal } from 'xterm';
 export async function openTerminal(): Promise<Terminal> {
     const terminal = new Terminal({ cols: 90, rows: 10, theme: { background: "#1a202c" }});
     terminal.open(document.getElementById('terminal')!);
+    terminal.clear();
     return terminal;
 }
