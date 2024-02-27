8d4645e51e68cad18fdd35d63fb2b30c9f1b74b1
Ben Meadors <benmmeadors@gmail.com>
2024-02-27 07:34:47 -0600

    Just remove em from the DOM

--- utils/terminal.ts
+++ utils/terminal.ts
@@ -1,8 +1,12 @@
 import { Terminal } from 'xterm';
 
 export async function openTerminal(): Promise<Terminal> {
+    // Remove any previous terminal elements from the DOM
+    const element = document.getElementById('terminal');
+    while (element?.firstChild) {
+        element.removeChild(element.firstChild);
+    }
     const terminal = new Terminal({ cols: 90, rows: 10, theme: { background: "#1a202c" }});
     terminal.open(document.getElementById('terminal')!);
-    terminal.clear();
     return terminal;
 }
