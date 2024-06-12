511b2aa36a66058df7f0284ad4a769821f345860
Ben Meadors <benmmeadors@gmail.com>
2024-06-11 20:32:32 -0500

    wio-tracker-wm1110

--- types/resources.ts
+++ types/resources.ts
@@ -330,7 +330,7 @@ export const OfflineHardwareList = [
     {
         "hwModel": 21,
         "hwModelSlug": "WIO_WM1110",
-        "platformioTarget": "wio-sdk-wm1110",
+        "platformioTarget": "wio-tracker-wm1110",
         "architecture": "nrf52840",
         "activelySupported": true,
         "displayName": "Seeed Wio WM1110 Tracker",
