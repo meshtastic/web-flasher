2907ee36cfdf82b5c8fdb56f80d7ae30a5d6af99
Ben Meadors <benmmeadors@gmail.com>
2024-08-20 15:58:02 -0500

    Smallen it

--- components/Firmware.vue
+++ components/Firmware.vue
@@ -4,7 +4,7 @@
             class="content-center text-black bg-meshtastic hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:bg-gray-500" 
             type="button"
             :disabled="!canSelectFirmware">
-            {{ selectedVersion.replace('Meshtastic Firmware ', '') }}
+            {{ selectedVersion.replace('Meshtastic Firmware ', '').replace('Technical ', '') }}
             <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
             </svg>
