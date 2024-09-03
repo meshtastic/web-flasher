0e353b5d0756c9a1b76f53be78e948fafc1ebd8a
Ben Meadors <benmmeadors@gmail.com>
2024-09-03 06:56:18 -0500

    Serve soft-device 7.3 specific erase uf2 to Seeed targets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,25 +4,31 @@ import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
   ssr: false,
   css: ['~/assets/css/main.css'],
+
   modules: [
     '@pinia/nuxt',
     '@vite-pwa/nuxt'
   ],
+
   pwa: {
     /* PWA options */
   },
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   vite: {
     plugins: [
       nodePolyfills(),
@@ -45,4 +51,6 @@ export default defineNuxtConfig({
       }
     }
   },
+
+  compatibilityDate: '2024-09-03',
 });
