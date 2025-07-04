9f55d0e847ef737c62cd224e43f57d5be72b876d
ReMatrixed <mat.vasenin@yandex.ru>
2025-07-04 13:50:53 +0300

    refactor: remove unused inline and add shrink-0

--- components/LanguagePicker.vue
+++ components/LanguagePicker.vue
@@ -3,7 +3,7 @@
     <button id="languageDropdownButton" data-dropdown-toggle="languageDropdown" type="button" 
             class="bottom-button border-meshtastic text-meshtastic">
       {{ currentLanguageFlag }} {{ $t('language') }}
-      <svg class="w-2.5 h-2.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <svg class="w-2.5 h-2.5 ml-2 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
       </svg>
     </button>
