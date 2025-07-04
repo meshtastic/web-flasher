7704d3bd6eba408b641a60ba33b939861ac49cef
ReMatrixed <mat.vasenin@yandex.ru>
2025-07-04 19:15:44 +0300

    refactor: replace standalone <svg> with XMarkIcon, remove redundant and invalid styles and optimize the classes usage in components/DeviceHeader.vue

--- components/DeviceHeader.vue
+++ components/DeviceHeader.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t order-gray-600">
+    <div class="flex items-center justify-between p-4 border-b rounded-t">
         <h3 class="text-xl font-semibold text-white">
             {{ $t('device.header') }}
         </h3>
         <button type="button"
-            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center hover:bg-gray-600 hover:text-white"
+            class="text-gray-400 rounded-lg text-sm size-8 p-1 flex justify-center items-center shrink-0 hover:bg-gray-600 hover:text-white"
             data-modal-toggle="device-modal">
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
+            <XMarkIcon />
             <span class="sr-only">{{ $t('actions.close_dialog') }}</span>
         </button>
     </div>
 </template>
+
+<script setup>
+import { XMarkIcon } from '@heroicons/vue/24/solid';
+</script>
