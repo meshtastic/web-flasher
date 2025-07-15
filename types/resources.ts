7d11278e8502ffe832de7abd1435225f72ba873f
Ben Meadors <benmmeadors@gmail.com>
2025-07-15 06:06:17 -0500

    2.7.3 pre-release

--- types/resources.ts
+++ types/resources.ts
@@ -3,29 +3,49 @@ import type { FirmwareResource } from './api';
 // Remove the OfflineHardwareList since it's now in /public/data/hardware-list.json
 
 const markdownContent = `
-
 ## 🚀 Enhancements
-* Fast fix, remove saving tx power inside limitPower() by @mrekin in https://github.com/meshtastic/firmware/pull/7255
-* Show user which Clock Face option is currently elected by @Xaositek in https://github.com/meshtastic/firmware/pull/7271
-* Heltec Wireless Paper, VM-E213 Hardware Revisions by @todd-herbert in https://github.com/meshtastic/firmware/pull/7258
+* Update Screen Wake Default Behavior by @Xaositek in https://github.com/meshtastic/firmware/pull/7282
+* Make the shouldWake function always available, and remove the bool by @jp-bennett in https://github.com/meshtastic/firmware/pull/7300
+* Shorter audio feedback for InkHUD buttons by @todd-herbert in https://github.com/meshtastic/firmware/pull/7301
+* Support native configuration Waveshare Pico LoRa module on Orange Pi Zero3 by @Mictronics in https://github.com/meshtastic/firmware/pull/7295
+* Load ringtone from userPrefs by @vidplace7 in https://github.com/meshtastic/firmware/pull/7298
+* Seesaw Rotary by @jp-bennett in https://github.com/meshtastic/firmware/pull/7310
+* GPS for STM32WL by @Stary2001 in https://github.com/meshtastic/firmware/pull/7297
+* Feat: add support for RAK Wismesh Tag hardware platform by @DanielCao0 in https://github.com/meshtastic/firmware/pull/6853
+* Message frame New Message Options and Clock / TDeck / Brightness Refinements by @Xaositek in https://github.com/meshtastic/firmware/pull/7344
 
 ## 🐛 Bug fixes and maintenance
-* Add HWIDs for T1000-E in DFU mode by @thebentern in https://github.com/meshtastic/firmware/pull/7235
-* chore(deps): update meshtastic/device-ui digest to 8c7092c by @renovate in https://github.com/meshtastic/firmware/pull/7238
-* Automatically bail user out of displaymode_color when not HAS_TFT by @jp-bennett in https://github.com/meshtastic/firmware/pull/7248
-* Don't run bluetooth gerFromRadio() unless the phone has requested a packet by @jp-bennett in https://github.com/meshtastic/firmware/pull/7231
-* Try-fix: L76K spamming bad times can crash nodes by @thebentern in https://github.com/meshtastic/firmware/pull/7261
-* Fix install script by @Pitel in https://github.com/meshtastic/firmware/pull/7259
-* Modules and favorite screen fix by @HarukiToreda in https://github.com/meshtastic/firmware/pull/7264
-* TFT_MESH Fixes Across Various Devices by @Xaositek in https://github.com/meshtastic/firmware/pull/7247
-* Update Bluetooth Toggle to match other variants by @Xaositek in https://github.com/meshtastic/firmware/pull/7269
-* Make PacketHistory logging less chatty by @thebentern in https://github.com/meshtastic/firmware/pull/7272
-* GitHub Actions faster!! (again) by @vidplace7 in https://github.com/meshtastic/firmware/pull/7268
+* Update RadioLib to v7.2.1 by @renovate[bot] in https://github.com/meshtastic/firmware/pull/7287
+* Update platformio.ini by @Kongduino in https://github.com/meshtastic/firmware/pull/7289
+* Update Adafruit BusIO to v1.17.2 by @renovate[bot] in https://github.com/meshtastic/firmware/pull/7277
+* Update dorny/test-reporter action to v2.1.1 by @renovate[bot] in https://github.com/meshtastic/firmware/pull/7284
+* Update meshtastic/device-ui digest to 404c6e0 by @renovate[bot] in https://github.com/meshtastic/firmware/pull/7302
+* Add first config override for Native by @jp-bennett in https://github.com/meshtastic/firmware/pull/7306
+* Update meshtastic/device-ui digest to 86a09a7 by @renovate[bot] in https://github.com/meshtastic/firmware/pull/7308
+* STM32: Properly ignore OneButton by @vidplace7 in https://github.com/meshtastic/firmware/pull/7311
+* Build: Update platformio with `pkg install` by @vidplace7 in https://github.com/meshtastic/firmware/pull/7315
+* Bump Framework-native and set version string. by @jp-bennett in https://github.com/meshtastic/firmware/pull/7317
+* userPrefs: Set default ringtone nag time by @vidplace7 in https://github.com/meshtastic/firmware/pull/7314
+* Remove Ubuntu oracular by @vidplace7 in https://github.com/meshtastic/firmware/pull/7322
+* feat: DIY Seeed XIAO nRF52840 + EBYTE E22 variants, pin-compatible with Wio-SX1262 kit by @ndoo in https://github.com/meshtastic/firmware/pull/7105
+* feat: New variant esp32c3_super_mini by @ndoo in https://github.com/meshtastic/firmware/pull/7133
+* xiao_ble README.md updates by @ndoo in https://github.com/meshtastic/firmware/pull/7283
+* fix(device-update.sh): safely filter args without breaking parsing by @NeilHanlon in https://github.com/meshtastic/firmware/pull/7305
+* NodeDB.cpp: Fix iOS bluetooth crash by ensuring UINT32_MAX is not used by @Styne13 in https://github.com/meshtastic/firmware/pull/7312
+* Improve OLED UI Responsiveness and Force Redraws for Canned message module by @csrutil in https://github.com/meshtastic/firmware/pull/7324
+* add pioenv to version string in debug log by @caveman99 in https://github.com/meshtastic/firmware/pull/7328
+* PPA: Add Ubuntu Questing (25.10) to daily builds by @vidplace7 in https://github.com/meshtastic/firmware/pull/7329
+* get git url part from local repo by @caveman99 in https://github.com/meshtastic/firmware/pull/7331
+* Add heap info via standard mallinfo() function for STM32 by @Stary2001 in https://github.com/meshtastic/firmware/pull/7327
+* The screen display of the heltec wireless tracker is abnormal. by @Quency-D in https://github.com/meshtastic/firmware/pull/7337
+* STM32 PlatformIO cleanup by @vidplace7 in https://github.com/meshtastic/firmware/pull/7339
+* Map report should work over devices which do not have network hardware (with client proxy) by @thebentern in https://github.com/meshtastic/firmware/pull/7341
+* Fix L1 EInk HWModel by @thebentern in https://github.com/meshtastic/firmware/pull/7346
 `;
 
-const currentPrereleaseId = '2.7.2.f6d3782';
+const currentPrereleaseId = '2.7.3.39716ed';
 
-export const showPrerelease = false;
+export const showPrerelease = true;
 
 export const currentPrerelease = <FirmwareResource>{
   id: `v${currentPrereleaseId}`,
