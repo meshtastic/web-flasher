860d84cb54030b774d045197074613d96df3b4f4
Ben Meadors <benmmeadors@gmail.com>
2025-07-19 06:34:22 -0500

    Remove pre-release

--- types/resources.ts
+++ types/resources.ts
@@ -48,7 +48,7 @@ const markdownContent = `
 
 const currentPrereleaseId = '2.7.3.71b6508';
 
-export const showPrerelease = true;
+export const showPrerelease = false;
 
 export const currentPrerelease = <FirmwareResource>{
   id: `v${currentPrereleaseId}`,
