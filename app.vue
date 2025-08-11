d41df35f7db37835ceef6a147ed27271a5ef14ae
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2025-08-11 15:30:10 +0200

    chore: update Inter font import URL to include italic and optical size variants

--- app.vue
+++ app.vue
@@ -186,7 +186,7 @@ onMounted(() => {
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..1000&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap');
   /* Additional Atkinson Hyperlegible fallback */
   @font-face {
