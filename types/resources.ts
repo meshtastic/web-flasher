7654d48e67f4d710d843b4f77c12542314076580
Ben Meadors <benmmeadors@gmail.com>
2025-09-28 06:48:14 -0500

    New device support

--- types/resources.ts
+++ types/resources.ts
@@ -83,4 +83,4 @@ export const currentPrerelease = <FirmwareResource>{
 };
 
 export const vendorCobrandingTag = "";
-export const supportedVendorDeviceTags = ["RAK", "B&Q", "LilyGo", "Seeed", "Heltec", "DIY", "Elecrow", "M5Stack"];
+export const supportedVendorDeviceTags = ["RAK", "B&Q", "LilyGo", "Seeed", "Heltec", "DIY", "Elecrow", "M5Stack", "NomadStar"];
