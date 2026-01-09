bfc76626231e267b84bce6702a6e45d26fdcace9
Ben Meadors <benmmeadors@gmail.com>
2026-01-09 06:28:51 -0600

    Uppercase

--- app.vue
+++ app.vue
@@ -340,6 +340,7 @@ onMounted(() => {
   }
   .title-doto {
     font-family: 'Doto', sans-serif;
+    text-transform: uppercase;
   }
   .bottom-button {
     @apply inline-flex items-center justify-center gap-2 px-5 py-2.5;
