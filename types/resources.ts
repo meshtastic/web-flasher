fe2b56d66456c319c9ac5bb015db52f94cd5590b
Ben Meadors <benmmeadors@gmail.com>
2026-03-11 06:53:06 -0500

    Set showPrerelease to false

--- types/resources.ts
+++ types/resources.ts
@@ -4,7 +4,7 @@ import type { FirmwareResource } from './api';
 
 const currentPrereleaseId = '2.7.20.6658ec2';
 
-export const showPrerelease = true;
+export const showPrerelease = false;
 
 export const currentPrerelease = <FirmwareResource>{
   id: `v${currentPrereleaseId}`,
