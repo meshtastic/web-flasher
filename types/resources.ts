de07df59733afe4a21583dc386b50574dbc7fb18
Ben Meadors <benmmeadors@gmail.com>
2026-04-02 07:00:42 -0500

    Update currentPrereleaseId and enable showPrerelease flag

--- types/resources.ts
+++ types/resources.ts
@@ -2,9 +2,9 @@ import type { FirmwareResource } from './api';
 
 // Remove the OfflineHardwareList since it's now in /public/data/hardware-list.json
 
-const currentPrereleaseId = '2.7.20.6658ec2';
+const currentPrereleaseId = '2.7.21.fc6c89a';
 
-export const showPrerelease = false;
+export const showPrerelease = true;
 
 export const currentPrerelease = <FirmwareResource>{
   id: `v${currentPrereleaseId}`,
